a4dc93054f06484baefa66af3da106a82755659b
RB-Codes <rasheedbader8@gmail.com>
2020-05-01 23:05:40 +0300

    Prompt page java

---
+++ Process.js
@@ -0,0 +1,13 @@
+var name1 = prompt('What is your first name?');
+var name2 = prompt('What is your last name?');
+
+
+alert("Hi " + name1 + " " + name2);
+
+var name3 = prompt('How old are you?');
+
+var answer = prompt('Do you know what we sell here?');
+
+function FBI() {
+    alert("FBI wants to know your location!");
+}
